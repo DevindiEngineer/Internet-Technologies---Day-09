35a81bfff2226241f46be0c7479c6bc05de8c0ab
DevindiEngineer <devindicampus@gmail.com>
2026-08-11 23:48:08 +0530

    Update project

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-console.log("Hii!!!");
+function sendPromtOnAction() {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application\\json");
+    myHeaders.append("X-goog-api-key", "APIKEY");
+
+    let userInput = document.getElementById("txtUserInput").value;
+
+    const raw = JSON.stringify({
+        contents: [{
+            parts: [{
+                text: userInput
+            }]
+        }]
+    });
+
+    const requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow"
+    };
+
+    fetch("https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent", requestOptions)
+        .then((response) => response.json())
+        .then((result) => {
+
+
+            document.getElementById("lblResponce").innerHTML=result.candidates[0].content.parts[0].text;
+        })
+        .catch((error) => console.error(error));
+}
